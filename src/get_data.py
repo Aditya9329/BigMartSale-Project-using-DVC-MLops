a8495f66c2d95dbc11d568c5c77df403079bd62b
Aditya9329 <maurya932926@gmail.com>
2022-10-31 10:24:06 -0700

    get_data.py and load_data.py is done

---
+++ src/get_data.py
@@ -0,0 +1,35 @@
+import os 
+import argparse
+import pandas as pd
+import yaml 
+
+
+def read_params(config_path):
+    with open(config_path) as yaml_file:
+        config = yaml.safe_load(yaml_file)
+    return config
+
+
+
+def get_data(config_path):
+    config = read_params(config_path)
+    data_path = config["data_source"]["s3_source"]
+    print(data_path)
+
+    df = pd.read_csv(data_path,sep=",")
+    print(df.head())
+    return df
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    args = argparse.ArgumentParser()
+    args.add_argument("--config",default = "params.yaml")
+    parsed_args = args.parse_args()
+    print(parsed_args.config)
+    print(parsed_args)
+    data = get_data(config_path = parsed_args.config)
