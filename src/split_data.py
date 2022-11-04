4e27e980140d9a5d1db963f54d22550aeb2b34da
Aditya9329 <maurya932926@gmail.com>
2022-11-03 19:10:58 -0700

    split data stage is completed

--- src/split_data.py
+++ src/split_data.py
@@ -10,12 +10,18 @@ from preprocessing import column_imputation,missing_imputation
 
 def split_and_save(config_path):
     config = read_params(config_path)
-    data_to_process = missing_imputation(config)
-    print(data_to_process)
-
+    train_data_path = config["split_data"]["train_path"]
+    test_data_path  = config["split_data"]["test_path"]
+    test_size       = config["split_data"]["test_size"]
+    random_state    = config["base"]["random_state"]
+    processed_data_path  = config["preprocessing"]["processed_data_path"]
+    df = pd.read_csv(processed_data_path,sep=",")
+    train,test  = train_test_split(df,test_size = test_size,random_state=random_state)
+    train.to_csv(train_data_path,sep=",",index=False)
+    test.to_csv(test_data_path,sep=",",index=False)
     
 
-
+    
 
 
 if __name__ == "__main__":
