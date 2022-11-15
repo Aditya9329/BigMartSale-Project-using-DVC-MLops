c3cd347fc01f5c09400aa7e1734666c533cbd5e7
Aditya9329 <maurya932926@gmail.com>
2022-11-15 07:04:45 -0800

    problem fixed

--- WebApp/application/views.py
+++ WebApp/application/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import jsonify
+import os
+import yaml
+import joblib
+import pickle
+import numpy as np
 
-# Create your views here.
+
+params_path = "../params.yaml"
+
+
+def home(request):
+    return HttpResponse("Hello")
